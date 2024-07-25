7ae967dc2b357e8b0cea299aeb7b2f71825f56c9
LI-MonnaiLAB <liyanchun2001@gmail.com>
2024-07-25 14:50:36 +0900

    install optoMDC

--- gstreamer_stream.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-def start_gstreamer_stream():
-    command = "raspivid -o - -t 0 -hf -w 640 -h 480 -fps 24 | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/}' :demux=h264"
-    subprocess.Popen(command, shell=True)
---
+++ oneMirror_Input_2axis_Angle.py
@@ -0,0 +1,48 @@
+import optoMDC
+import time
+import math
+from AngleOutput import get_phi_theta
+
+# 角度からXY座標への変換関数
+def angle_to_xy(angle):
+    return math.tan(math.radians(angle)) / math.tan(math.radians(50))
+
+# ユーザー入力をクランプする関数
+def clamp(value, min_value, max_value):
+    return max(min_value, min(value, max_value))
+
+# COMポート定義
+port = "COM4"
+
+# optoMDCライブラリを通じてデバイスに接続
+mre2 = optoMDC.connect(port=port)
+
+
+# Channel_0 (X軸) の制御モードをXYモード（Closed Loop）に設定
+ch_0 = mre2.Mirror.Channel_0
+ch_0.SetControlMode(optoMDC.Units.XY)
+static_input_0 = ch_0.StaticInput
+
+# Channel_1 (Y軸) の制御モードをXYモード（Closed Loop）に設定
+ch_1 = mre2.Mirror.Channel_1
+ch_1.SetControlMode(optoMDC.Units.XY)
+static_input_1 = ch_1.StaticInput
+
+try:
+    for phi_value, theta_value in get_phi_theta():
+        if phi_value is None or theta_value is None:
+            break
+
+        xy_value_x = clamp(angle_to_xy(phi_value), -1, 1)
+        xy_value_y = clamp(angle_to_xy(theta_value), -1, 1)
+
+        static_input_0.SetXY(xy_value_x)
+        static_input_1.SetXY(xy_value_y)
+
+        # 設定後、0.1秒待機
+        time.sleep(0.1)
+except KeyboardInterrupt:
+    pass
+
+# デバイスから切断
+mre2.disconnect()
